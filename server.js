3a4b63b58b4d6641e9a874ce7fc2baf8c8043e56
𝔄𝔩𝔢𝔵𝔞𝔫𝔡𝔢𝔯 <222906509+Alextheloner@users.noreply.github.com>
2026-01-16 09:36:24 +0100

    minor change to welcome message in dashboard

--- server.js
+++ server.js
@@ -20,8 +20,16 @@ app.get("/", (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
+const auth = require("./middleware/authMiddleware");
+
+app.get("/api/dashboard", auth, (req, res) => {
+  res.json({
+    msg: "Welcome to CampusHub ",
+    userId: req.user.id,
+  });
+});
